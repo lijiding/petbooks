499987cb72aed8be155bcecbab0234ac8d74c18f
AlreadyM <617054896@qq.com>
2018-05-02 21:34:19 +0800

    add source file

--- source/js/normal.js
+++ source/js/normal.js
@@ -23,3 +23,70 @@ $('.cancle-change-city').on('click',function () {
 $('body').on('click',function (argument) {
 	$('.place-select').removeClass('selecting');
 })
+
+/**
+ * [usrLogin 用户登录 注册功能切换]
+ * @return {[type]} [null]
+ */
+function usrLogin() {
+    var login = $('.usr-login'),
+        tabs = login.find('.tab'),
+        panels = login.find('.panel'),
+        cn = 'active';
+    tabs.each(function(i, el) {
+        $(el).bind('click', function() {
+            tabs.removeClass(cn)
+            $(this).addClass(cn)
+            panels.hide()
+            panels.eq(i).show()
+        })
+    })
+}
+usrLogin()
+/**
+ * [toggleSigninWay 切换登录类型]
+ * @return {[type]} [ null]
+ */
+function toggleSigninWay() {
+    $('.normal,.phone-sigin').on('click',function () {
+        $('.change-sigin-way').toggleClass('phone');
+        $('.sigin-info').toggleClass('phone');
+    })
+}
+toggleSigninWay()
+
+// login popup
+function loginPopup(html) {
+    var mask = "<div class='login-popup-mask'></div>",
+        content = html,
+        popup = "<div class='login-popup-fun'>"+mask+"<div class='login-popup'><div class='login-pannel usr-login'>"+content+"</div></div></div>";
+        console.log(popup)
+        console.log(html)
+    $('body').append(popup);
+    $('.login-popup-mask').on('click',function () {
+        $('.login-popup-fun').remove()
+    })
+
+    // init login
+    usrLogin()
+    toggleSigninWay()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
